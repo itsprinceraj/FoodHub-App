07485982ec6e7a8f72e2483a246a1d92602cfc3d
devops-prince <dev.prince116@gmailcom>
2024-05-09 00:49:50 +0530

    verel added

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./.env" });
 // const FRONTEND_URL = process.env.FRONTEND_URL;
 app.use(
   cors({
-    origin: "https://food-hub-app-delta.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
